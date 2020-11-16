faf71fe28e94fe2da63a529afa75a303432de055
Rafal Kulik <rafal.kulik@intel.com>
2020-11-16 20:10:33 +0100

    isa 3 completed

--- students/src/main/java/pl/edu/eti/pg/lab/IsaStudentsApplication.java
+++ students/src/main/java/pl/edu/eti/pg/lab/IsaStudentsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IsaStudentsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IsaStudentsApplication.class, args);
 	}
