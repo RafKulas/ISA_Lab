636b5eacdd272c08473c48abd1ac9cfcc3d3d88c
Rafal Kulik <rafal.kulik@intel.com>
2020-11-16 13:30:25 +0100

    isa 3 update settings.gradle

---
+++ students/src/main/java/pl/edu/eti/pg/lab/IsaStudentsApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.eti.pg.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaStudentsApplication.class, args);
+	}
+
+}
