a18045b33dde8218912d87cc68890b1e5e5cf1ca
Rafal Kulik <rafal.kulik@intel.com>
2022-09-13 09:56:04 +0200

    update dockers

--- students/src/main/java/pl/edu/eti/pg/lab/faculty/dto/CreateFacultyRequest.java
+++ students/src/main/java/pl/edu/eti/pg/lab/faculty/dto/CreateFacultyRequest.java
@@ -1,25 +1,21 @@
 package pl.edu.eti.pg.lab.faculty.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import pl.edu.eti.pg.lab.faculty.entity.Faculty;
 
 import java.util.function.Function;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateFacultyRequest {
 	private String name;
 
 	public static Function<CreateFacultyRequest, Faculty> dtoToEntityMapper() {
 		return request -> new Faculty(request.getName());
-	}
-
-	public CreateFacultyRequest(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 }
